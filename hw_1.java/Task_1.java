1ad8d0f67644064ceb934550d2770eb284f54fd8
Olga <olgajensen@mail.ru>
2023-03-12 22:52:15 +0300

    Вычислить n-ое треугольного число(сумма чисел от 1 до n), а так же n! (произведение чисел от 1 до n)

--- hw_1.java/Task_1.java
+++ hw_1.java/Task_1.java
@@ -1,5 +1,26 @@
+// 1. Вычислить n-ое треугольного числа (сумма чисел от 1 до n), 
+// а так же n! (произведение чисел от 1 до n)
+// Ввод:5
+// Треугольное число 1 + 2 + 3 + 4 + 5 = 15
+// n! 1 * 2 * 3 * 4 * 5 = 120
+
+import java.util.Scanner;
 
 public class Task_1 {
-    
-    
+    public static void main(String[] args) {
+        System.out.printf("Введите число n: ");
+        Scanner iScanner = new Scanner(System.in, "Cp866"); 
+        int num = iScanner.nextInt();
+        int triangularNum = 0;
+        int factorialNum = 1;
+        for (int i = 1; i <= num; i++) {
+            triangularNum += i;
+            factorialNum *= i;
+        }
+        System.out.printf("Треугольное число: %s\n", triangularNum);
+        System.out.printf("Факториал числа: %s\n", factorialNum);
+        iScanner.close(); 
+                    
+    }
 }
+
