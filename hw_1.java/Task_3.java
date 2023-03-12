888a055ba999e1b9bc14be65af5fb8db796d679d
Olga <olgajensen@mail.ru>
2023-03-13 02:32:52 +0300

    Реализовать простой калькулятор (+ - / *)

---
+++ hw_1.java/Task_3.java
@@ -0,0 +1,36 @@
+// 3. Реализовать простой калькулятор (+ - / *)
+// Ввод числа ->
+// Ввод знака ->
+// Ввод числа ->
+
+import java.util.Scanner;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in, "Cp866");
+        System.out.println("Введите первок число: ");
+        int num1 = iScanner.nextInt();
+        System.out.println("Введите оператор: +, -, /, * ");
+        char operator = iScanner.next().charAt(0);
+        System.out.println("Введите второе число: ");
+        int num2 = iScanner.nextInt();
+        int result = 0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+        }
+        System.out.printf("%s %s %s = %s", num1, operator, num2, result);
+        iScanner.close(); 
+    }
+}
